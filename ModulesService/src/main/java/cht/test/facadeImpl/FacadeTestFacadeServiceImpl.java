b4ed3d32fdc5ef6044dad1b795626a668dd46562
amanic <1642485710@qq.com>
2018-05-30 13:40:43 +0800

    add dubbo provider module

---
+++ ModulesService/src/main/java/cht/test/facadeImpl/FacadeTestFacadeServiceImpl.java
@@ -0,0 +1,14 @@
+package cht.test.facadeImpl;
+
+import cht.test.facade.FacadeTestFacadeService;
+
+/**
+ * Created by chenhaitao on 2018/5/30.
+ */
+public class FacadeTestFacadeServiceImpl implements FacadeTestFacadeService{
+
+
+    public String test(String s) {
+        return null;
+    }
+}
---
+++ facade/src/main/java/cht/test/facade/FacadeTestFacadeService.java
@@ -0,0 +1,9 @@
+package cht.test.facade;
+
+/**
+ * Created by chenhaitao on 2018/5/30.
+ */
+public interface FacadeTestFacadeService {
+
+    String test(String s);
+}
